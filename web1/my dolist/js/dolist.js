c4b9727327b5d49fea6716338101f47da9c1cdff
Jerffeson Gomes <jerffeson.gomes@imd.ufrn.br>
2019-04-01 20:30:11 -0300

    finalizando o editar

--- web1/my dolist/js/dolist.js
+++ web1/my dolist/js/dolist.js
@@ -1,13 +1,16 @@
 class Atividade {
 	nomeResponsavel;
 	prazo;
+	situacao;
 
 	constructor(nomeResponsavel, prazo){
 		this.nomeResponsavel = nomeResponsavel;
 		this.prazo = prazo;
+		this.situacao = 0;
 	}
 
 }
+
 class Projeto {
     nome;
     atividades= [];
@@ -24,40 +27,55 @@ var listProjetos = [];
 function addProjeto()  {
 
 	var projeto = new Projeto($("#nome-projeto").val());
-	listProjetos.push(projeto);
+	var indexProjeto = $("#index-projeto").val();
+
+	if(indexProjeto == null ||  indexProjeto == "") {
+		listProjetos.push(projeto);
+		$("#list-projetos").append(prepararProjeto(projeto, (listProjetos.length-1)));
+	} else if (indexProjeto >=0 && indexProjeto < listProjetos.length) {
+		listProjetos[indexProjeto].nome = projeto.nome;
+		atualizarListagemProjetos();
+	}
 	
 
-	$("#list-projetos").append(prepararProjeto(projeto, (listProjetos.length-1)));
 	$("#nome-projeto").val("");
 	$("#modalAddProjeto").modal('hide');
 
 }
 
+function editarProjeto (indexProjeto) {
+
+	$("#modalAddProjeto").modal('show');
+
+	$("#nome-projeto").val(listProjetos[indexProjeto].nome);
+	$("#index-projeto").val(indexProjeto);
+
+
+}
 
 function deletarProjeto(indexProjeto)  {
 
-	console.log(listProjetos);
 	listProjetos.splice(indexProjeto, 1);
+	atualizarListagemProjetos()
+
+}
+
+
+function atualizarListagemProjetos() {
+
 	$("#list-projetos").html('');
 
 	$.each(listProjetos, function( index, projeto ) {
 		$("#list-projetos").append(prepararProjeto(projeto, index));
 	});
-
-	console.log(listProjetos);
-
 }
 
+function visualizarAtividades() {
 
-function editarProjeto () {
-
-	
 
 }
-
 
 function prepararProjeto(projeto, indexProjeto) {
-
 
 	return '<div class="col-4 m-4 border border-blue rounded" style="min-width: 350px; max-width: 350px;"><div class="card-body"><header class="row p-0">'+
 	'<h4 class="card-title col-7">' + projeto.nome + '</h4><div class="col-5 p-0 text-right"><button type="button" onclick="deletarProjeto('+indexProjeto+')" class=" btn btn-primary p-1"><i class="fas fa-trash-alt text-danger"></i></button>'+
