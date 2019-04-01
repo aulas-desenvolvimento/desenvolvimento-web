f9e753d30278c84280a4ef4773527a55b60094c0
= <=>
2019-03-31 21:13:47 -0300

    Subindo novo projeto: Add e delete de projetos OK, iniciei o editar

--- web1/my dolist/js/dolist.js
+++ web1/my dolist/js/dolist.js
@@ -1,24 +1,68 @@
+class Atividade {
+	nomeResponsavel;
+	prazo;
 
-function addProjeto()  {
+	constructor(nomeResponsavel, prazo){
+		this.nomeResponsavel = nomeResponsavel;
+		this.prazo = prazo;
+	}
 
-	console.log($("#nome-projeto").val());
+}
+class Projeto {
+    nome;
+    atividades= [];
 
-	$("#list-projetos").append(prepararProjeto($("#nome-projeto").val()));
-
-	$("#nome-projeto").val("");
-	$("#modalAddProjeto").modal('hide');
+    constructor(nome){
+		this.nome = nome;
+	}
 
 
 }
 
+var listProjetos = [];
 
-function prepararProjeto(nomeProjeto) {
+function addProjeto()  {
+
+	var projeto = new Projeto($("#nome-projeto").val());
+	listProjetos.push(projeto);
+	
+
+	$("#list-projetos").append(prepararProjeto(projeto, (listProjetos.length-1)));
+	$("#nome-projeto").val("");
+	$("#modalAddProjeto").modal('hide');
+
+}
+
+
+function deletarProjeto(indexProjeto)  {
+
+	console.log(listProjetos);
+	listProjetos.splice(indexProjeto, 1);
+	$("#list-projetos").html('');
+
+	$.each(listProjetos, function( index, projeto ) {
+		$("#list-projetos").append(prepararProjeto(projeto, index));
+	});
+
+	console.log(listProjetos);
+
+}
+
+
+function editarProjeto () {
+
+	
+
+}
+
+
+function prepararProjeto(projeto, indexProjeto) {
 
 
 	return '<div class="col-4 m-4 border border-blue rounded" style="min-width: 350px; max-width: 350px;"><div class="card-body"><header class="row p-0">'+
-	'<h4 class="card-title col-7">' + nomeProjeto + '</h4><div class="col-5 p-0 text-right"><a href="#" class="p-1"><i class="fas fa-trash-alt text-danger"></i></a>'+
+	'<h4 class="card-title col-7">' + projeto.nome + '</h4><div class="col-5 p-0 text-right"><button type="button" onclick="deletarProjeto('+indexProjeto+')" class=" btn btn-primary p-1"><i class="fas fa-trash-alt text-danger"></i></button>'+
 	'</div></header><div class="row justify-content-center">'+
 	'<a class="btn btn-primary col-5 mr-1" href="#"><i class="fas fa-eye"></i> ver</a>'+
-	'<a class="btn btn-primary col-5" href="#"><i class="fas fa-edit"></i> Editar</a></div></div></div>';
+	'<button class="btn btn-primary col-5" onclick="editarProjeto('+indexProjeto+')" ><i class="fas fa-edit"></i> Editar</button></div></div></div>';
 
 }
