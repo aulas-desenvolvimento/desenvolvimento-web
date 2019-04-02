248b813aa639dd2204d44bb9a7d2102829d2eccb
Jerffeson Gomes <jerffeson.gomes@imd.ufrn.br>
2019-04-02 19:37:31 -0300

    Fazendo drag e drop, parece q esta ok

--- web1/my dolist/js/dolist.js
+++ web1/my dolist/js/dolist.js
@@ -1,9 +1,11 @@
 class Atividade {
+	titulo
 	nomeResponsavel;
 	prazo;
 	situacao;
 
-	constructor(nomeResponsavel, prazo){
+	constructor(titulo, nomeResponsavel, prazo){
+		this.titulo = titulo;
 		this.nomeResponsavel = nomeResponsavel;
 		this.prazo = prazo;
 		this.situacao = 0;
@@ -18,17 +20,16 @@ class Projeto {
     constructor(nome){
 		this.nome = nome;
 	}
-
-
 }
 
 var listProjetos = [];
+var projetoListado = -1;
 
 function addProjeto()  {
 
 	var projeto = new Projeto($("#nome-projeto").val());
 	var indexProjeto = $("#index-projeto").val();
-
+	console.log(indexProjeto);
 	if(indexProjeto == null ||  indexProjeto == "") {
 		listProjetos.push(projeto);
 		$("#list-projetos").append(prepararProjeto(projeto, (listProjetos.length-1)));
@@ -50,7 +51,6 @@ function editarProjeto (indexProjeto) {
 	$("#nome-projeto").val(listProjetos[indexProjeto].nome);
 	$("#index-projeto").val(indexProjeto);
 
-
 }
 
 function deletarProjeto(indexProjeto)  {
@@ -60,6 +60,46 @@ function deletarProjeto(indexProjeto)  {
 
 }
 
+function visualizarAtividades(indexProjeto) {
+
+	$("#titulo-nome-projeto").html(listProjetos[indexProjeto].nome);
+	atualizarListagemAtividades(indexProjeto);
+	projetoListado = indexProjeto;
+}
+
+function addAtividade()  {
+
+
+	if (projetoListado == -1) {
+		alert('Primeiro você precisa visualizar um projeto.') ;
+	} else {
+
+		var atividade = new Atividade($("#input-titulo-atividade").val(), $("#input-responsavel-atividade").val(), $("#input-prazo-atividade").val());
+		var indexAtividade = $("#index-atividade").val();
+
+		if(indexAtividade == null || indexAtividade == "") {
+
+			listProjetos[projetoListado].atividades.push(atividade);
+			atualizarListagemAtividades(projetoListado);
+
+		} else if (indexProjeto >=0 && indexProjeto < listProjetos.length) {
+			listProjetos[projetoListado].atividades[indexAtividade].titulo = atividade.titulo;
+			listProjetos[projetoListado].atividades[indexAtividade].nomeResponsavel = atividade.nomeResponsavel;
+			listProjetos[projetoListado].atividades[indexAtividade].prazo = atividade.prazo;
+			listProjetos[projetoListado].atividades[indexAtividade].situacao = atividade.situacao;
+			atualizarListagemProjetos(projetoListado);
+		}
+		
+		$("#input-titulo-atividade").val("");
+		$("#input-responsavel-atividade").val("");
+		$("#input-prazo-atividade").val("");
+		$("#index-atividade").val("");
+
+	}
+	
+	$("#modalAddAtividade").modal('hide');
+
+}
 
 function atualizarListagemProjetos() {
 
@@ -70,9 +110,24 @@ function atualizarListagemProjetos() {
 	});
 }
 
-function visualizarAtividades() {
+function atualizarListagemAtividades(indexProjeto) {
+
+	$("#list-afazer").html('');
+	$("#list-fazendo").html('');
+	$("#list-feita").html('');
 
 
+	$.each(listProjetos[indexProjeto].atividades, function( index, atividade ) {
+
+		if(atividade.situacao == 0) {
+			$("#list-afazer").append(prepararAtividade(atividade, index));
+		} else if (atividade.situacao == 1) {
+			$("#list-fazendo").append(prepararAtividade(atividade, index));
+		} else if (atividade.situacao == 2) {
+			$("#list-feita").append(prepararAtividade(atividade, index));
+		}
+		
+	});
 }
 
 function prepararProjeto(projeto, indexProjeto) {
@@ -80,7 +135,45 @@ function prepararProjeto(projeto, indexProjeto) {
 	return '<div class="col-4 m-4 border border-blue rounded" style="min-width: 350px; max-width: 350px;"><div class="card-body"><header class="row p-0">'+
 	'<h4 class="card-title col-7">' + projeto.nome + '</h4><div class="col-5 p-0 text-right"><button type="button" onclick="deletarProjeto('+indexProjeto+')" class=" btn btn-primary p-1"><i class="fas fa-trash-alt text-danger"></i></button>'+
 	'</div></header><div class="row justify-content-center">'+
-	'<a class="btn btn-primary col-5 mr-1" href="#"><i class="fas fa-eye"></i> ver</a>'+
+	'<button class="btn btn-primary col-5 mr-1" onclick="visualizarAtividades('+indexProjeto+')" ><i class="fas fa-eye"></i> ver</button>'+
 	'<button class="btn btn-primary col-5" onclick="editarProjeto('+indexProjeto+')" ><i class="fas fa-edit"></i> Editar</button></div></div></div>';
 
+}
+
+
+function prepararAtividade(atividade, indexAtividade) {
+
+	return '<div draggable="true" ondragstart="drag(event)" id="'+indexAtividade+'" class="row border atividade"><div class="col-12" ><header class="row p-0"><span clas="titulo-tarefa">'+ atividade.titulo +'</span><div class="col-5 p-0 text-right">'+
+	'<a href="#" class="p-1"><i class="fas fa-edit"></i></a><a href="#" class="p-1"><i class="fas fa-trash-alt text-danger"></i></a></div></header>'+
+	'<div class="row text-secondary data-responsavel">'+atividade.prazo+' - '+atividade.nomeResponsavel+'</div></div></div>';
+
+}
+
+function atualizarSituacaoAtividade(indexAtividade, idLista) {
+
+	if (idLista == "list-afazer") {
+		listProjetos[projetoListado].atividades[indexAtividade].situacao = 0;
+	} else if (idLista == "list-fazendo") {
+		listProjetos[projetoListado].atividades[indexAtividade].situacao = 1;
+	} else if (idLista == "list-feita") {
+		listProjetos[projetoListado].atividades[indexAtividade].situacao = 2;
+	}
+
+	atualizarListagemAtividades(projetoListado);
+
+}
+
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev, lista) {
+
+	var indexAtividade = ev.dataTransfer.getData("text");
+	atualizarSituacaoAtividade(indexAtividade, $(lista).attr('id'));
+  
 }
